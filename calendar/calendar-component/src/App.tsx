0faa3f3985256a3847b3dc6fceca5455a1747874
airbowen <airbowen@163.com>
2024-08-14 16:51:07 +0800

    final version

--- calendar/calendar-component/src/App.tsx
+++ calendar/calendar-component/src/App.tsx
@@ -1,18 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Calendar from './Calendar';
 import dayjs from 'dayjs';
+import Calendar from './Calendar';
 
 function App() {
   return (
     <div className="App">
-      <Calendar value={dayjs('2023-11-1')}></Calendar>
+      <Calendar value={dayjs('2023-11-08')}></Calendar>
     </div>
   );
 }
 
 export default App;
-
-
-
